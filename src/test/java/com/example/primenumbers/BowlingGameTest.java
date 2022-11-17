d6586a14ce4104f92ca17d6c3fd2529568f1347c
Jakub <kubasuchanowski@gmail.com>
2022-11-17 02:02:25 +0100

    poprawione

--- src/test/java/com/example/primenumbers/BowlingGameTest.java
+++ src/test/java/com/example/primenumbers/BowlingGameTest.java
@@ -19,19 +19,23 @@ public class BowlingGameTest {
     }
 
     @Test
+    //zwraca zero jesli w każdym rzucie chybimy
     public void testZero() {
         rollMany(20, 0, g);
         assertEquals(0, g.score());
     }
 
+    //powinno zwrocic 20 jesli w kazdym rzucie trafimy po jednym kręglu
     @Test
     public void testAllOnes() {
         rollMany(20, 1, g);
         assertEquals(20, g.score());
     }
 
+
+    //powinno zwrocic 16 kiedy w pierwszej turze trafimy spare, a nastpępnie trzy kręgle
     @Test
-    public void testOneSpare() {
+    public void shouldReturnSixTeenWhenSpareIsKnockDown() {
         g.roll(5);
         g.roll(5);
         g.roll(3);
@@ -39,6 +43,7 @@ public class BowlingGameTest {
         assertEquals(16, g.score());
     }
 
+    //jesli w pierwszym rzucie jest strike, następnie 3 i 4 kręgle to powinno wyjsc 24
     @Test
     public void testOneStrike() {
         g.roll(10);
@@ -48,6 +53,8 @@ public class BowlingGameTest {
         assertEquals(24, g.score());
     }
 
+
+    //jesli w kazdym rzucie strike to powinno byc 300
     @Test
     public void testPerfectGame() {
         rollMany(12, 10, g);
